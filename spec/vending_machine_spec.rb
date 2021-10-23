a1a2f36d59958e6e236f1ac0324bc2123c0cdd69
Andrii Furmanets <afurm@Andriis-MacBook-Air.local>
2021-10-23 11:42:13 +0300

    init

---
+++ spec/vending_machine_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe VendingMachine do
+  it "has a version number" do
+    expect(VendingMachine::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
